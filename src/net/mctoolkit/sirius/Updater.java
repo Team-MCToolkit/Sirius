b8fff2af15aaf4f42fac36038299b3ba8f16cdbc
Goldorion <toutankamon@outlook.com>
2021-03-01 14:51:25 -0500

    Add methods to start the downloading and the unzipping

--- src/net/mctoolkit/sirius/Updater.java
+++ src/net/mctoolkit/sirius/Updater.java
@@ -20,18 +20,25 @@ import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 
-public class Updater {
+public class Updater extends JFrame{
 
     public static Updater INSTANCE;
+    public UpdaterInfo info;
     private JProgressBar bar;
     private JProgressBar bar2;
 
     public Updater(UpdaterInfo info) {
         INSTANCE = this;
+        this.info = info;
 
-        String dest = info.getDestFile().getPath();
+        setResizable(false);
+        setSize(new Dimension(330, 87));
+        setLocationRelativeTo(null);
+        setCursor(Cursor.WAIT_CURSOR);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 
-        JFrame frame = createJFrame();
+    public void startDownloading() {
         try {
             bar = new JProgressBar(JProgressBar.HORIZONTAL, 0, info.getUrl().openConnection().getContentLength());
         } catch (IOException exception) {
@@ -39,40 +46,32 @@ public class Updater {
         }
         bar.setValue(0);
         bar.setStringPainted(true);
-        frame.add(bar);
-        frame.setVisible(true);
-        frame.setTitle("Downloading...");
-        FileUtils.downloadFile(info.getUrl(), new File(dest));
+        add(bar);
+        setVisible(true);
+        setTitle("Downloading...");
+        FileUtils.downloadFile(info.getUrl(), new File(info.getDestFile().getPath()));
+    }
 
+    public void startUnzipping() {
+        setTitle("Unzipping...");
+        String dest = info.getDestFile().getPath();
         File zipFile = new File(dest);
-        File destFile = new File(dest.replace(info.getNameZip() + ".zip", ""));
-        frame.setTitle("Unzipping...");
-
         bar2 = new JProgressBar(JProgressBar.HORIZONTAL, 0, (int) (zipFile.length() / (1024 * 1024)));
         bar2.setValue(0);
         bar2.setStringPainted(true);
         bar.setVisible(false);
-        frame.add(bar2);
-        FileUtils.unzip(dest, destFile);
+        add(bar2);
+        FileUtils.unzip(dest, new File(dest.replace(info.getNameZip() + ".zip", "")));
         zipFile.delete();
+
+        //When we have unzipped the file and deleted it, we say to the user to close the program.
         String[] options = {"Close"};
-        int option = JOptionPane.showOptionDialog(frame,
+        int option = JOptionPane.showOptionDialog(this,
                 "The installation is completed." + "\nPlease restart " + info.getAppName() + " to apply changes.",
                 "Installation completed", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, null,
                 options, options[0]);
         if (option == 0)
             System.exit(0);
-    }
-
-    private static JFrame createJFrame() {
-        JFrame frame = new JFrame();
-        frame.setResizable(false);
-        frame.setSize(new Dimension(330, 87));
-        frame.setLocationRelativeTo(null);
-        frame.setCursor(Cursor.WAIT_CURSOR);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        return frame;
     }
 
     public JProgressBar getBar() {
